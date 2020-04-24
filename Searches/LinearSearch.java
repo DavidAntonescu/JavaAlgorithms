44ead388b239d0f64469ff26a4c032ffcce928ca
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-24 13:15:29 +0300

    Update LinearSearch.java

--- Searches/LinearSearch.java
+++ Searches/LinearSearch.java
@@ -3,31 +3,23 @@ package Searches;
 import java.util.Random;
 import java.util.stream.Stream;
 
-/**
- * Linear search is the easiest search algorithm
+/* Linear search is the easiest search algorithm
  * It works with sorted and unsorted arrays (an binary search works only with sorted array)
  * This algorithm just compares all elements of an array to find a value
- * <p>
  * Worst-case performance	O(n)
  * Best-case performance	O(1)
  * Average performance	O(n)
  * Worst-case space complexity
- *
- * @author Varun Upadhyay (https://github.com/varunu28)
- * @author Podshivalov Nikita (https://github.com/nikitap492)
  * @see BinarySearch
  * @see SearchAlgorithm
  */
 
 public class LinearSearch implements SearchAlgorithm {
 
-    /**
-     * Generic Linear search method
-     *
+    /* Generic Linear search method
      * @param array List to be searched
      * @param value Key being searched for
-     * @return Location of the key
-     */
+     * @return Location of the key*/
     @Override
     public <T extends Comparable<T>> int find(T[] array, T value) {
         for (int i = 0; i < array.length; i++) {
