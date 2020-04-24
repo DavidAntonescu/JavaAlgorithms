44db32923d9e7844f06b10a6a3baca6efd160e8d
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-24 14:13:42 +0300

    Update ColumnarTranspositionCipher.java

--- Ciphers/ColumnarTranspositionCipher.java
+++ Ciphers/ColumnarTranspositionCipher.java
@@ -1,10 +1,7 @@
 package ciphers;
 
-/**
- * Columnar Transposition Cipher Encryption and Decryption.
- *
- * @author <a href="https://github.com/freitzzz">freitzzz</a>
- */
+/*Columnar Transposition Cipher Encryption and Decryption.*/
+
 public class ColumnarTranspositionCipher {
 
     private static String keyword;
@@ -15,14 +12,12 @@ public class ColumnarTranspositionCipher {
     private static final String ENCRYPTION_FIELD = "≈";
     private static final char ENCRYPTION_FIELD_CHAR = '≈';
 
-    /**
-     * Encrypts a certain String with the Columnar Transposition Cipher Rule
-     *
+    /* Encrypts a certain String with the Columnar Transposition Cipher Rule
      * @param word Word being encrypted
      * @param keyword String with keyword being used
      * @return a String with the word encrypted by the Columnar Transposition
-     * Cipher Rule
-     */
+     * Cipher Rule*/
+    
     public static String encrpyter(String word, String keyword) {
         ColumnarTranspositionCipher.keyword = keyword;
         abecedariumBuilder(500);
@@ -37,16 +32,14 @@ public class ColumnarTranspositionCipher {
         return wordEncrypted;
     }
 
-    /**
-     * Encrypts a certain String with the Columnar Transposition Cipher Rule
-     *
+    /*Encrypts a certain String with the Columnar Transposition Cipher Rule
      * @param word Word being encrypted
      * @param keyword String with keyword being used
      * @param abecedarium String with the abecedarium being used. null for
      * default one
      * @return a String with the word encrypted by the Columnar Transposition
-     * Cipher Rule
-     */
+     * Cipher Rule*/
+    
     public static String encrpyter(String word, String keyword,
             String abecedarium) {
         ColumnarTranspositionCipher.keyword = keyword;
@@ -66,13 +59,11 @@ public class ColumnarTranspositionCipher {
         return wordEncrypted;
     }
 
-    /**
-     * Decrypts a certain encrypted String with the Columnar Transposition
+    /*Decrypts a certain encrypted String with the Columnar Transposition
      * Cipher Rule
-     *
      * @return a String decrypted with the word encrypted by the Columnar
-     * Transposition Cipher Rule
-     */
+     * Transposition Cipher Rule*/
+    
     public static String decrypter() {
         String wordDecrypted = "";
         for (int i = 1; i < table.length; i++) {
@@ -83,13 +74,11 @@ public class ColumnarTranspositionCipher {
         return wordDecrypted.replaceAll(ENCRYPTION_FIELD, "");
     }
 
-    /**
-     * Builds a table with the word to be encrypted in rows by the Columnar
-     * Transposition Cipher Rule
-     *
-     * @return An Object[][] with the word to be encrypted filled in rows and
-     * columns
-     */
+    /*Builds a table with the word to be encrypted in rows by the Columnar
+     *Transposition Cipher Rule
+     *@return An Object[][] with the word to be encrypted filled in rows and
+     *columns*/
+    
     private static Object[][] tableBuilder(String word) {
         Object[][] table = new Object[numberOfRows(word) + 1][keyword.length()];
         char[] wordInChards = word.toCharArray();
@@ -109,13 +98,11 @@ public class ColumnarTranspositionCipher {
         return table;
     }
 
-    /**
-     * Determines the number of rows the table should have regarding the
-     * Columnar Transposition Cipher Rule
-     *
-     * @return an int with the number of rows that the table should have in
-     * order to respect the Columnar Transposition Cipher Rule.
-     */
+    /*Determines the number of rows the table should have regarding the
+     *Columnar Transposition Cipher Rule
+     *@return an int with the number of rows that the table should have in
+     *order to respect the Columnar Transposition Cipher Rule.*/
+    
     private static int numberOfRows(String word) {
         if ((double) word.length() / keyword.length() > word.length() / keyword.length()) {
             return (word.length() / keyword.length()) + 1;
@@ -124,10 +111,8 @@ public class ColumnarTranspositionCipher {
         }
     }
 
-    /**
-     *
-     * @return charValues
-     */
+    /*@return charValues*/
+    
     private static Object[] findElements() {
         Object[] charValues = new Object[keyword.length()];
         for (int i = 0; i < charValues.length; i++) {
@@ -137,11 +122,9 @@ public class ColumnarTranspositionCipher {
         return charValues;
     }
 
-    /**
-     *
-     * @param table
-     * @return tableSorted
-     */
+    /*@param table
+     *@return tableSorted*/
+    
     private static Object[][] sortTable(Object[][] table) {
         Object[][] tableSorted = new Object[table.length][table[0].length];
         for (int i = 0; i < tableSorted.length; i++) {
@@ -158,13 +141,11 @@ public class ColumnarTranspositionCipher {
         return tableSorted;
     }
 
-    /**
-     *
-     * @param table
-     * @param rows
-     * @param column
-     * @return columnArray
-     */
+    /*@param table
+     *@param rows
+     *@param column
+     *@return columnArray*/
+    
     private static Object[] getColumn(Object[][] table, int rows, int column) {
         Object[] columnArray = new Object[rows];
         for (int i = 0; i < rows; i++) {
@@ -173,13 +154,11 @@ public class ColumnarTranspositionCipher {
         return columnArray;
     }
 
-    /**
-     *
-     * @param table
-     * @param firstColumnIndex
-     * @param secondColumnIndex
-     * @param columnToSwitch
-     */
+    /*@param table
+     *@param firstColumnIndex
+     *@param secondColumnIndex
+     *@param columnToSwitch*/
+    
     private static void switchColumns(Object[][] table, int firstColumnIndex,
             int secondColumnIndex, Object[] columnToSwitch) {
         for (int i = 0; i < table.length; i++) {
@@ -188,11 +167,9 @@ public class ColumnarTranspositionCipher {
         }
     }
 
-    /**
-     * Creates an abecedarium with a specified ascii inded
-     *
-     * @param value Number of characters being used based on the ASCII Table
-     */
+    /*Creates an abecedarium with a specified ascii inded
+     *@param value Number of characters being used based on the ASCII Table*/
+    
     private static void abecedariumBuilder(int value) {
         abecedarium = "";
         for (int i = 0; i < value; i++) {
