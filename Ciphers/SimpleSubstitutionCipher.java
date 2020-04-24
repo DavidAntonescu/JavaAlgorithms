a02f93ed8dcce77f9cf2edf8f76bd74cae776056
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-24 14:16:44 +0300

    Update SimpleSubstitutionCipher.java

--- Ciphers/SimpleSubstitutionCipher.java
+++ Ciphers/SimpleSubstitutionCipher.java
@@ -2,26 +2,18 @@ package ciphers;
 
 import java.util.*;
 
-/**
- *
- * The simple substitution cipher is a cipher that has been in use for many hundreds of years 
- * (an excellent history is given in Simon Singhs 'the Code Book'). 
- * It basically consists of substituting every plaintext character for a different ciphertext character. 
- * It differs from the Caesar cipher in that the cipher alphabet is not simply the alphabet shifted,
- * it is completely jumbled.
- * 
- * @author Hassan Elseoudy
- */
+/*The simple substitution cipher is a cipher that has been in use for many hundreds of years 
+ *It basically consists of substituting every plaintext character for a different ciphertext character. 
+ *It differs from the Caesar cipher in that the cipher alphabet is not simply the alphabet shifted,
+ *it is completely jumbled.*/
 
 public class SimpleSubstitutionCipher {
 
-    /**
-     * Encrypt text by replacing each element with its opposite character.
-     *
-     * @param message
-     * @param cipherSmall
-     * @return Encrypted message
-     */
+    /*Encrypt text by replacing each element with its opposite character.
+     *@param message
+     *@param cipherSmall
+     *@return Encrypted message*/
+    
     public static String encode(String message, String cipherSmall) {
         String encoded = "";
 
@@ -50,13 +42,11 @@ public class SimpleSubstitutionCipher {
         return encoded;
     }
 
-    /**
-     * Decrypt message by replacing each element with its opposite character in cipher.
-     *
-     * @param encryptedMessage
-     * @param cipherSmall
-     * @return message
-     */
+    /*Decrypt message by replacing each element with its opposite character in cipher.
+     *@param encryptedMessage
+     *@param cipherSmall
+     *@return message*/
+    
     public static String decode(String encryptedMessage, String cipherSmall) {
         String decoded = "";
 
@@ -84,10 +74,8 @@ public class SimpleSubstitutionCipher {
         return decoded;
     }
 
-    /**
-     *
-     * TODO remove main and make JUnit Testing
-     */
+    /*TODO remove main and make JUnit Testing*/
+    
     public static void main(String[] args) {
         String a = encode("defend the east wall of the castle","phqgiumeaylnofdxjkrcvstzwb");
         String b = decode(a,"phqgiumeaylnofdxjkrcvstzwb");
