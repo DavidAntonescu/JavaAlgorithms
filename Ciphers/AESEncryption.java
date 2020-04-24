cb3144fe8109c245e03c4107ced043a0b0724c3f
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-24 14:07:57 +0300

    Update AESEncryption.java

--- Ciphers/AESEncryption.java
+++ Ciphers/AESEncryption.java
@@ -9,23 +9,18 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
-/**
- * This example program shows how AES encryption and decryption can be done in
+/* This example program shows how AES encryption and decryption can be done in
  * Java. Please note that secret key and encrypted text is unreadable binary and
- * hence in the following program we display it in hexadecimal format of the
- * underlying bytes.
- * 
- */
+ * hence in the following program we display it in hexadecimal format of the underlying bytes.*/
+
 public class AESEncryption {
 
 
 	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-	/**
-	 * 1. Generate a plain text for encryption 2. Get a secret key (printed in
-	 * hexadecimal form). In actual use this must by encrypted and kept safe. The
-	 * same key is required for decryption.
-	 * 
-	 */
+	/*1. Generate a plain text for encryption 2. Get a secret key (printed in
+	 * hexadecimal form). In actual use this must by encrypted and kept safe.
+	 * The same key is required for decryption.*/
+	
 	public static void main(String[] args) throws Exception {
 		String plainText = "Hello World";
 		SecretKey secKey = getSecretEncryptionKey();
@@ -39,15 +34,11 @@ public class AESEncryption {
 
 	}
 
-	/**
-	 * gets the AES encryption key. In your actual programs, this should be safely
-	 * stored.
-	 * 
+	/*gets the AES encryption key. In your actual programs, this should be safely stored.
 	 * @return secKey (Secret key that we encrypt using it)
 	 * @throws NoSuchAlgorithmException
-	 *             (from KeyGenrator)
-	 * 
-	 */
+	 * (from KeyGenrator)*/
+	
 	public static SecretKey getSecretEncryptionKey() throws NoSuchAlgorithmException {
 		KeyGenerator aesKeyGenerator = KeyGenerator.getInstance("AES");
 		aesKeyGenerator.init(128); // The AES key size in number of bits
@@ -55,23 +46,21 @@ public class AESEncryption {
 		return secKey;
 	}
 
-	/**
-	 * Encrypts plainText in AES using the secret key
-	 * 
+	/*Encrypts plainText in AES using the secret key
 	 * @param plainText
 	 * @param secKey
 	 * @return byteCipherText (The encrypted text)
 	 * @throws NoSuchPaddingException
-	 *             (from Cipher)
+	 * (from Cipher)
 	 * @throws NoSuchAlgorithmException
-	 *             (from Cipher)
+	 * (from Cipher)
 	 * @throws InvalidKeyException
-	 *             (from Cipher)
+	 * (from Cipher)
 	 * @throws BadPaddingException
-	 *             (from Cipher)
+	 * (from Cipher)
 	 * @throws IllegalBlockSizeException
-	 *             (from Cipher)
-	 */
+	 * (from Cipher)*/
+	
 	public static byte[] encryptText(String plainText, SecretKey secKey) throws NoSuchAlgorithmException,
 			NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		// AES defaults to AES/ECB/PKCS5Padding in Java 7
@@ -81,9 +70,7 @@ public class AESEncryption {
 		return byteCipherText;
 	}
 
-	/**
-	 * Decrypts encrypted byte array using the key used for encryption.
-	 * 
+	/*Decrypts encrypted byte array using the key used for encryption.
 	 * @param byteCipherText
 	 * @param secKey
 	 * @return plainText
@@ -91,8 +78,8 @@ public class AESEncryption {
 	 * @throws NoSuchAlgorithmException
 	 * @throws InvalidKeyException
 	 * @throws BadPaddingException
-	 * @throws IllegalBlockSizeException
-	 */
+	 * @throws IllegalBlockSizeException*/
+	
 	public static String decryptText(byte[] byteCipherText, SecretKey secKey) throws NoSuchAlgorithmException,
 			NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		// AES defaults to AES/ECB/PKCS5Padding in Java 7
@@ -102,17 +89,16 @@ public class AESEncryption {
 		return new String(bytePlainText);
 	}
 
-	/**
-	 * Convert a binary byte array into readable hex form
+	/* Convert a binary byte array into readable hex form
 	 * Old library is deprecated on OpenJdk 11 and 
 	 * this is faster regarding other solution is using StringBuilder
 	 * Credit
 	 * {@link 
 	 * https://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java/9855338#9855338}
 	 * @param hash
-	 *            (in binary)
-	 * @return hexHash
-	 */
+	 * (in binary)
+	 * @return hexHash*/
+	
 	public static String bytesToHex(byte[] bytes) {
 	    char[] hexChars = new char[bytes.length * 2];
 	    for (int j = 0; j < bytes.length; j++) {
