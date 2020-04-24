08f260fe14af68627c2c261bd45e061052cf692d
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-24 14:15:07 +0300

    Update RSA.java

--- Ciphers/RSA.java
+++ Ciphers/RSA.java
@@ -4,18 +4,12 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 import javax.swing.JOptionPane;
 
-/**
- * @author Nguyen Duy Tiep on 23-Oct-17.
- */
 public final class RSA {
 
-    /**
-     * Trivial test program.
-     *
-     * @param args
-     * @deprecated TODO remove main and make JUnit Testing or any other
-     * methodology
-     */
+    /*Trivial test program.
+     *@param args
+     *@deprecated TODO remove main and make JUnit Testing or any other methodology*/
+    
     public static void main(String[] args) {
 
         RSA rsa = new RSA(1024);
@@ -29,55 +23,43 @@ public final class RSA {
 
     private BigInteger modulus, privateKey, publicKey;
 
-    /**
-     *
-     * @param bits
-     */
+    /*@param bits*/
+    
     public RSA(int bits) {
         generateKeys(bits);
     }
 
-    /**
-     *
-     * @param message
-     * @return encrypted message
-     */
+    /*@param message
+     *@return encrypted message*/
+    
     public synchronized String encrypt(String message) {
         return (new BigInteger(message.getBytes())).modPow(publicKey, modulus).toString();
     }
 
-    /**
-     *
-     * @param message
-     * @return encrypted message as big integer
-     */
+    /*@param message
+     *@return encrypted message as big integer*/
+    
     public synchronized BigInteger encrypt(BigInteger message) {
         return message.modPow(publicKey, modulus);
     }
 
-    /**
-     *
-     * @param encryptedMessage
-     * @return plain message
-     */
+    /*@param encryptedMessage
+     *@return plain message*/
+    
     public synchronized String decrypt(String encryptedMessage) {
         return new String((new BigInteger(encryptedMessage)).modPow(privateKey, modulus).toByteArray());
     }
 
-    /**
-     *
-     * @param encryptedMessage
-     * @return plain message as big integer
-     */
+    /*@param encryptedMessage
+     *@return plain message as big integer*/
+    
     public synchronized BigInteger decrypt(BigInteger encryptedMessage) {
         return encryptedMessage.modPow(privateKey, modulus);
     }
 
-    /**
-     * Generate a new public and private key set.
-     *
-     * @param bits
-     */
+    /*Generate a new public and private key set.
+     *@param bits*/
+    
     public synchronized void generateKeys(int bits) {
         SecureRandom r = new SecureRandom();
         BigInteger p = new BigInteger(bits / 2, 100, r);
