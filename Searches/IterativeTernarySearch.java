a02d4202038fcd0a3626563a36fcbfaffb77304b
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-24 13:10:54 +0300

    Update IterativeTernarySearch.java

--- Searches/IterativeTernarySearch.java
+++ Searches/IterativeTernarySearch.java
@@ -6,8 +6,7 @@ import java.util.stream.Stream;
 
 import static java.lang.String.format;
 
-/*
- * A iterative version of a ternary search algorithm
+/* A iterative version of a ternary search algorithm
  * This is better way to implement the ternary search, because a recursive version adds some overhead to a stack.
  * But in java the compile can transform the recursive version to iterative implicitly,
  * so there are no much differences between these two algorithms
