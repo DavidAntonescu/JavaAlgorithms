20b7281b9ed001c3bf94e7a9b427493bde46b98e
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-25 16:37:48 +0300

    Update MaxHeap.java

--- DataStructures/Heaps/MaxHeap.java
+++ DataStructures/Heaps/MaxHeap.java
@@ -3,12 +3,8 @@ package DataStructures.Heaps;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Heap tree where a node's key is higher than or equal to its parent's and lower than or equal
- * to its children's.
- *
- * @author Nicolas Renard
- */
+
+/*Heap tree where a node's key is higher than or equal to its parent's and lower than or equal to its children's.*/
 public class MaxHeap implements Heap {
 
     private final List<HeapElement> maxHeap;
@@ -22,12 +18,8 @@ public class MaxHeap implements Heap {
         if (maxHeap.size() == 0) System.out.println("No element has been added, empty heap.");
     }
 
-    /**
-     * Get the element at a given index. The key for the list is equal to index value - 1
-     *
-     * @param elementIndex index
-     * @return heapElement
-     */
+    
+    /*Get the element at a given index. The key for the list is equal to index value - 1 */
     public HeapElement getElement(int elementIndex) {
         if ((elementIndex <= 0) || (elementIndex > maxHeap.size()))
             throw new IndexOutOfBoundsException("Index out of heap range");
