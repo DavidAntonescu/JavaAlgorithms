3bfb0de7533a4756786a9c1e9fc75dd4836d94c3
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-25 15:55:01 +0300

    Update GCD.java

--- Maths/GCD.java
+++ Maths/GCD.java
@@ -3,7 +3,7 @@ package Maths;
 /*This is Euclid's algorithm which is used to find the greatest common denominator*/
 public class GCD {
 
-    /*Get greatest common divisor*/
+    /*Gets the greatest common divisor*/
     public static int gcd(int num1, int num2) {
         if (num1 < 0 || num2 < 0) {
             throw new ArithmeticException();
