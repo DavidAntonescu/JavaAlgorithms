dd2afdc84662eaa80d11a731174ca12d9f815fb2
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-25 14:50:24 +0300

    Update IterativeBinarySearch.java

--- Searches/IterativeBinarySearch.java
+++ Searches/IterativeBinarySearch.java
@@ -6,12 +6,10 @@ import java.util.stream.Stream;
 
 import static java.lang.String.format;
 
-/*
- * Binary search is one of the most popular algorithms
+/* Binary search is one of the most popular algorithms
  * This class represents iterative version  {@link BinarySearch}
  * Iterative binary search is likely to have lower constant factors because it doesn't involve the overhead of manipulating the call stack.
  * But in java the recursive version can be optimized by the compiler to this version.
- * <p>
  * Worst-case performance	O(log n)
  * Best-case performance	O(1)
  * Average performance	O(log n)
@@ -23,11 +21,11 @@ import static java.lang.String.format;
 public final class IterativeBinarySearch implements SearchAlgorithm {
 
     /* This method implements an iterative version of binary search algorithm
-     *
      * @param array a sorted array
      * @param key   the key to search in array
      * @return the index of key in the array or -1 if not found
-     */
+    */
+    
     @Override
     public <T extends Comparable<T>> int find(T[] array, T key) {
         int l, r, k, cmp;
