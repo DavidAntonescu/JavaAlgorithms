cfdab06cd99d4755b8f5d70cae6e12e204421f08
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-25 15:46:32 +0300

    Update ConnectedComponent.java

--- DataStructures/Graphs/ConnectedComponent.java
+++ DataStructures/Graphs/ConnectedComponent.java
@@ -4,11 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * A class that counts the number of different connected components in a graph
- *
- * @author Lukas Keul, Florian Mercks
- */
+/*A class that counts the number of different connected components in a graph*/
 class Graph<E extends Comparable<E>> {
 
     class Node {
@@ -36,13 +32,7 @@ class Graph<E extends Comparable<E>> {
         nodeList = new ArrayList<Node>();
     }
 
-    /**
-     * Adds a new Edge to the graph. If the nodes aren't yet in nodeList, they
-     * will be added to it.
-     *
-     * @param startNode the starting Node from the edge
-     * @param endNode   the ending Node from the edge
-     */
+    /*Adds a new Edge to the graph. If the nodes aren't yet in nodeList, they will be added to it.*/
     public void addEdge(E startNode, E endNode) {
         Node start = null, end = null;
         for (Node node : nodeList) {
@@ -64,14 +54,10 @@ class Graph<E extends Comparable<E>> {
         edgeList.add(new Edge(start, end));
     }
 
-    /**
-     * Main method used for counting the connected components. Iterates through
+    /* Main method used for counting the connected components. Iterates through
      * the array of nodes to do a depth first search to get all nodes of the
      * graph from the actual node. These nodes are added to the array
-     * markedNodes and will be ignored if they are chosen in the nodeList.
-     *
-     * @return returns the amount of unconnected graphs
-     */
+     * markedNodes and will be ignored if they are chosen in the nodeList.*/
     public int countGraphs() {
         int count = 0;
         Set<Node> markedNodes = new HashSet<Node>();
@@ -87,13 +73,7 @@ class Graph<E extends Comparable<E>> {
         return count;
     }
 
-    /**
-     * Implementation of depth first search.
-     *
-     * @param n       the actual visiting node
-     * @param visited A list of already visited nodes in the depth first search
-     * @return returns a set of visited nodes
-     */
+    /*Implementation of depth first search.*/
     public ArrayList<Node> depthFirstSearch(Node n, ArrayList<Node> visited) {
         visited.add(n);
         for (Edge e : edgeList) {
