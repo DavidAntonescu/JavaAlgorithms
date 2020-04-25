b8f69df14a5b469356a4a892e5fb17429482976b
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-25 16:12:09 +0300

    Update EditDistance.java

--- DynamicProgramming/EditDistance.java
+++ DynamicProgramming/EditDistance.java
@@ -1,23 +1,7 @@
 package DynamicProgramming;
 
-/**
- * A DynamicProgramming based solution for Edit Distance problem In Java
- * Description of Edit Distance with an Example:
- * <p>
- * Edit distance is a way of quantifying how dissimilar two strings (e.g., words) are to one another,
- * by counting the minimum number of operations required to transform one string into the other. The
- * distance operations are the removal, insertion, or substitution of a character in the string.
- * <p>
- * <p>
- * The Distance between "kitten" and "sitting" is 3. A minimal edit script that transforms the former into the latter is:
- * <p>
- * kitten → sitten (substitution of "s" for "k")
- * sitten → sittin (substitution of "i" for "e")
- * sittin → sitting (insertion of "g" at the end).
- *
- * @author SUBHAM SANGHAI
- **/
 
+/* A DynamicProgramming based solution for the Edit Distance problem In Java*/
 import java.util.Scanner;
 
 public class EditDistance {
@@ -27,7 +11,7 @@ public class EditDistance {
         int len2 = word2.length();
         // len1+1, len2+1, because finally return dp[len1][len2]
         int[][] dp = new int[len1 + 1][len2 + 1];
-     	/* If second string is empty, the only option is to
+     	/* If the second string is empty, the only option is to
    	  insert all characters of first string into second*/
         for (int i = 0; i <= len1; i++) {
             dp[i][0] = i;
