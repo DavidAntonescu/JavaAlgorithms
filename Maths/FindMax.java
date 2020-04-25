0e2fdf98e7af95d8b04e41d4aa3b01abd516c801
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-25 15:50:49 +0300

    Update FindMax.java

--- Maths/FindMax.java
+++ Maths/FindMax.java
@@ -8,12 +8,7 @@ public class FindMax {
         System.out.println("max = " + findMax(array));
     }
 
-    /**
-     * find max of array
-     *
-     * @param array the array contains element
-     * @return max value
-     */
+    /*Find max of the array*/
     public static int findMax(int[] array) {
         int max = array[0];
         for (int i = 1; i < array.length; ++i) {
