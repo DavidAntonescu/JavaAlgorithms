bcee65f75b9d61862358ea8ca755fa06e22c3398
Michael <58954060+michaelraduu@users.noreply.github.com>
2020-04-25 15:27:57 +0300

    Update WorstFit.java

--- Others/WorstFit.java
+++ Others/WorstFit.java
@@ -2,20 +2,15 @@ package Others;
 
 import java.util.ArrayList;
 
-/**
- * @author Dekas Dimitrios
- */
 public class WorstFit {
     private static final int NO_ALLOCATION = -255; // if a process has been allocated in position -255,
                                                    // it means that it has not been actually allocated.
 
-    /**
-     * Method to find the index of the memory block that is going to fit the given process based on the worst fit algorithm.
-     *
+    /* Method to find the index of the memory block that is going to fit the given process based on the worst fit algorithm.
      * @param blocks: the array with the available memory blocks.
      * @param process: the size of the process.
-     * @return the index of the block that fits, or -255 if no such block exists.
-     */
+     * @return the index of the block that fits, or -255 if no such block exists.*/
+    
     private static int findWorstFit(int[] blockSizes, int processSize) {
         int max = -1;
         int index = -1;
@@ -32,16 +27,14 @@ public class WorstFit {
         return index;
     }
 
-    /**
-     * Method to allocate memory to blocks according to the worst fit
+    /* Method to allocate memory to blocks according to the worst fit
      * algorithm. It should return an ArrayList of Integers, where the
      * index is the process ID (zero-indexed) and the value is the block
      * number (also zero-indexed).
-     *
      * @param sizeOfBlocks: an int array that contains the sizes of the memory blocks available.
      * @param sizeOfProcesses: an int array that contains the sizes of the processes we need memory blocks for.
-     * @return the ArrayList filled with Integers repressenting the memory allocation that took place.
-     */
+     * @return the ArrayList filled with Integers repressenting the memory allocation that took place.*/
+    
     static ArrayList<Integer> worstFit(int[] sizeOfBlocks, int[] sizeOfProcesses) {
         // The array list responsible for saving the memory allocations done by the worst-fit algorithm
         ArrayList<Integer> memAlloc = new ArrayList<>();
@@ -56,11 +49,9 @@ public class WorstFit {
         return memAlloc;
     }
 
-    /**
-     * Method to print the memory allocated.
-     *
-     * @param memAllocation: an ArrayList of Integer representing the memory allocation done by the worstFit method.
-     */
+    /*Method to print the memory allocated.
+     * @param memAllocation: an ArrayList of Integer representing the memory allocation done by the worstFit method.*/
+    
     public static void printMemoryAllocation(ArrayList<Integer> memAllocation) {
         System.out.println("Process No.\tBlock No.");
         System.out.println("===========\t=========");
